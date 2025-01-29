8e3b09df158f100011f360edd53b0d34f9fbf3d4
yuva kishore <yuvakishorekolli@gmail.com>
2025-01-29 15:40:37 +0530

    updated the styles

--- qtify/src/components/AlbumCard/AlbumCard.jsx
+++ qtify/src/components/AlbumCard/AlbumCard.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography ,Chip} from "@mui/material";
+import { Card, CardContent, CardMedia, Typography, Chip } from "@mui/material";
 
 const AlbumCard = ({ data }) => {
   return (
-    <Card sx={{ fontFamily:'Poppins-Medium', height: 250, maxWidth: 345 ,width:159 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={data.image} // Assuming `coverImage` is the album cover URL
-        alt={data.title}
-      />
-      <CardContent>
-        <Typography style={{fontFamily:'Poppins'}} variant="h6" component="div">
-          {data.title} {/* Album Title */}
-        </Typography>
-        <Chip style={{fontFamily:'Poppins' , color:'white' ,background:'black'}} label={`${data.follows} follows`} />
-      </CardContent>
-    </Card>
+    <>
+      <Card
+        sx={{
+          fontFamily: "Poppins-Medium",
+          height: 250,
+          maxWidth: 345,
+          width: 159,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="200"
+          image={data.image} // Assuming `coverImage` is the album cover URL
+          alt={data.title}
+        />
+        <CardContent>
+          <Chip
+            style={{
+              fontFamily: "Poppins",
+              color: "white",
+              background: "black",
+            }}
+            label={`${data.follows} follows`}
+          />
+        </CardContent>
+      </Card>
+      <Typography
+        mb={2}
+        mt={3}
+        color={"white"}
+        variant="h7"
+        className="album-name"
+        fontFamily={"Poppins"}
+      >
+        {data.title}
+      </Typography>
+    </>
   );
 };
 
